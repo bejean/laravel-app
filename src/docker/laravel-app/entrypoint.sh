2a5b494c6b9e2b006ce7ee4ace8416983fcef531
Bejean <dominique.bejean@eolya.fr>
2020-09-24 15:57:02 +0200

    Docker - Automatisation installation packages optionnels

--- src/docker/laravel-app/entrypoint.sh
+++ src/docker/laravel-app/entrypoint.sh
@@ -6,7 +6,17 @@ then
 	cd  /tmp
 	laravel new $LARAVELAPP
 	cp -rf $LARAVELAPP /var/www/.
-	chown -R $LARAVELUSER: /var/www/$LARAVELAPP
+	chown -R $LARAVELUSER: /var/www/$LARAVELAPP	
+
+	chmod +x /tmp/init-scripts/*.sh
+	cd /var/www/$LARAVELAPP
+
+	for f in /tmp/init-scripts/*.sh; do
+		#echo $f >> /tmp/scripts.txt
+  		bash "$f" -H || break  # execute successfully or break
+  		# Or more explicitly: if this execution fails, then stop the `for`:
+  		# if ! bash "$f" -H; then break; fi
+	done
 fi
 
 apache2-foreground
