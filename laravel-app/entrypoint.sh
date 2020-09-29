095cab8c136df535764cf034ca319bacb173dc68
Bejean <dominique.bejean@eolya.fr>
2020-09-29 15:48:38 +0200

    Docker

--- laravel-app/entrypoint.sh
+++ laravel-app/entrypoint.sh
@@ -7,15 +7,19 @@ then
 	laravel new $LARAVELAPP
 	cp -rf $LARAVELAPP /var/www/.
 
-	chmod +x /tmp/init-scripts/*.sh
 	cd /var/www/$LARAVELAPP
+	sed  -i "/DB_HOST/c\DB_HOST=${LARAVELAPP}_laravel_db" .env
+	sed  -i "/DB_DATABASE/c\DB_DATABASE=$LARAVELAPP" .env
+	sed  -i "/DB_USERNAME/c\DB_USERNAME=$LARAVELAPP" .env
+	sed  -i "/DB_PASSWORD/c\DB_PASSWORD=$LARAVELAPP" .env
 
-    if ls /tmp/init-scripts/*.sh 1> /dev/null 2>&1; then
+	count=`ls -1 /tmp/init-scripts/*.sh 2>/dev/null | wc -l`
+	if [ $count != 0 ]    
+	then
+		chmod +x /tmp/init-scripts/*.sh
     	for f in /tmp/init-scripts/*.sh; do
 	    	echo "execute $f" >> /tmp/scripts.txt
   		    bash "$f" -H || break  # execute successfully or break
-  		    # Or more explicitly: if this execution fails, then stop the `for`:
-  		    # if ! bash "$f" -H; then break; fi
 	    done
 	fi
 
